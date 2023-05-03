1adbe024e5f9c1a58ca616e6197a871e81e8d155
AndrixPro100GOD <55459745+AndrixPro100GOD@users.noreply.github.com>
2023-05-04 02:47:48 +0500

    + Event Script
    Added TestEvent Script
    Small Map Edited

---
+++ Assets/Game/Scripts/Events/TestAction.cs
@@ -0,0 +1,114 @@
+using Game.Player;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Game.Scripts.Events
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class TestAction : MonoBehaviour
+    {
+        [SerializeField]
+        private TMPro.TextMeshPro interactionText;
+
+        [SerializeField]
+        private UnityEvent onInteraction;
+
+        [SerializeField]
+        private bool isSingleUse = true;
+
+        [SerializeField, HideInInspector]
+        private Transform _interactionViewer;
+
+        [SerializeField, HideInInspector]
+        private BoxCollider2D _boxCollider;
+
+#if UNITY_EDITOR
+
+        private void Reset()
+        {
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _boxCollider.isTrigger = true;
+        }
+
+        private void OnValidate()
+        {
+            _boxCollider = _boxCollider != null ? GetComponent<BoxCollider2D>() : _boxCollider;
+
+            if (_boxCollider != null)
+                _boxCollider.isTrigger = true;
+
+            if (interactionText != null)
+                _interactionViewer = interactionText.transform;
+        }
+
+#endif
+
+        private Transform tempPlayer;
+        private Transform playerCamera;
+        private bool _isPlayerInteracting = false;
+        private bool _isUsed = false;
+
+        private void Awake()
+        {
+            playerCamera = FindObjectOfType<Camera>()?.transform;//Тут типа можно за место камеры любой скрипт поставить
+
+            if (interactionText == null)
+                return;
+
+            _interactionViewer.gameObject.SetActive(false);
+            _interactionViewer.eulerAngles = Vector3.up * 90f;
+        }
+
+        private void Update()
+        {
+            if (!_isPlayerInteracting)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _isUsed = true;
+
+                if (isSingleUse)
+                    RemoveAction();
+
+                Debug.Log("ACTION");
+                onInteraction?.Invoke();
+            }
+
+            if (interactionText == null)
+                return;
+            _interactionViewer.position = GetPositionToPlaceViewer();
+            _interactionViewer.LookAt(playerCamera, -Vector3.up);
+        }
+
+        private void RemoveAction()
+        {
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out PlayerController player))
+            {
+                tempPlayer = player.transform;
+                _interactionViewer.gameObject.SetActive(true);
+                _isPlayerInteracting = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out PlayerController _))
+            {
+                _interactionViewer.gameObject.SetActive(false);
+                _isPlayerInteracting = false;
+            }
+        }
+
+        private Vector3 GetPositionToPlaceViewer()
+        {
+            return tempPlayer == null ? Vector3.zero : _boxCollider.bounds.ClosestPoint(tempPlayer.position) + Vector3.up;
+        }
+    }
+}
