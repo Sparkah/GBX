05ee5e7320e669300aee966528ca6913e2a18eff
AndrixPro100GOD <55459745+AndrixPro100GOD@users.noreply.github.com>
2023-05-04 23:15:57 +0500

    added background changer
    small map edit
    + script background changer
    + sprite

---
+++ Assets/Game/Scripts/Experemental/BackgroundChanger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Game.Scripts.Experemental
+{
+    public class BackgroundChanger : MonoBehaviour
+    {
+        [SerializeField]
+        private Image imageRenderer;
+
+        [SerializeField]
+        private List<Sprite> sprites = new List<Sprite>();
+
+        private int index = 0;
+
+        private void Awake()
+        {
+            imageRenderer.sprite = sprites[index];
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                NextBackground();
+            }
+        }
+
+        private void NextBackground()
+        {
+            index += 1;
+            if (index > sprites.Count - 1)
+                index = 0;
+
+            imageRenderer.sprite = sprites[index];
+        }
+    }
+}
